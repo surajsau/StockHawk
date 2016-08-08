eab6ea3da13dcd2f5b62c405272e3af9b2762b2b
surajsau <suraj.sau@snapdeal.com>
2016-08-08 15:37:55 +0530

    Refacroting code stage - 1

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/LineGraphActivity.java
@@ -0,0 +1,9 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by surajkumarsau on 08/08/16.
+ */
+public class LineGraphActivity extends AppCompatActivity {
+}
